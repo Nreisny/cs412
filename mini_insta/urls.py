b890cd2ad5e1eab30b62310503f514ed2b86c2cb
Nicholas Reis <nreisny@bu.edu>
2026-05-26 08:36:35 -0400

    Mini_insta checkpoint commit

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -3,5 +3,6 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-
+ path(r"", views.ProfileListView.as_view(), name="show_all_profile"),
+ path(r"profile/<int:pk>", views.ProfileDetailView.as_view(), name="show_profile")
 ]
--- mini_insta/views.py
+++ mini_insta/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from .models import Profile
+from django.views.generic import ListView, DetailView
 # Create your views here.
+class ProfileListView(ListView):
+    '''Defines a class to view all profiles'''
+
+    model = Profile
+    template_name = "mini_insta/show_all_profiles.html"
+    context_object_name = "profiles"
+
+class ProfileDetailView(DetailView):
+    '''Defines a class to view a single profile'''
+    model = Profile
+    template_name = "mini_insta/show_profile.html"
+    context_object_name = "profile"
